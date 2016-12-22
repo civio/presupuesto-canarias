209697c07ea78e055b746824c6efe649424edb01
Eduardo Sebastian <esebastian@altiora.es>
2016-12-22 08:33:19 +0100

    Update data source links

--- settings.py
+++ settings.py
@@ -93,13 +93,13 @@ FACEBOOK_ID             = '1421631951469772'
 ANALYTICS_ID            = 'UA-28946840-26'
 
 # Setup Data Source Budget link
-DATA_SOURCE_BUDGET      = ''
+DATA_SOURCE_BUDGET      = 'http://www.gobiernodecanarias.org/hacienda/dgplani/presupuestos/'
 
 # Setup Data Source Population link
-DATA_SOURCE_POPULATION  = ''
+DATA_SOURCE_POPULATION  = 'http://www.ine.es/jaxiT3/Tabla.htm?t=2853&L=0'
 
 # Setup Data Source Inflation link
-DATA_SOURCE_INFLATION   = ''
+DATA_SOURCE_INFLATION   = 'http://www.ine.es/jaxiT3/Tabla.htm?t=10019&L=0'
 
 # Setup Main Entity Web Url
 MAIN_ENTITY_WEB_URL     = 'http://www.gobiernodecanarias.org/'
