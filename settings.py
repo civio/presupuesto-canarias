890b53588bebf56eaf7ae37eebce5a6b54828f18
Eduardo Sebastian <esebastian@altiora.es>
2016-12-22 08:33:49 +0100

    Configure featured programmes

--- settings.py
+++ settings.py
@@ -125,7 +125,7 @@ MAIN_ENTITY_LEGAL_URL   = 'http://www.gobiernodecanarias.org/principal/avisolega
 # ----------------------
 
 # Programmes to feature as example in home page.
-FEATURED_PROGRAMMES = ['1621', '1710', '3380', '2410', '3110']
+FEATURED_PROGRAMMES = ['132B', '231I', '241C', '311A', '312A']
 
 # Number of programmes to feature in home page. Default: 3
 # NUMBER_OF_FEATURED_PROGRAMMES = 3
